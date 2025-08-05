dd38b44bf98f798cceaafec5373529b832f917ab
김용해 <yoho1019@gmail.com>
2025-08-05 11:31:41 +0900

    Initial Commit

---
+++ ClipMate/ClipMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipMateApp.swift
+//  ClipMate
+//
+//  Created by 김용해 on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipMate
+//
+//  Created by 김용해 on 8/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
