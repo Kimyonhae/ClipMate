f8c0e491bcd7c6689a74eb590d3b495786cbc15d
김용해 <yoho1019@gmail.com>
2025-08-07 23:58:17 +0900

    feat: up, down keyboard 조작을 통해 clip 이동

--- ClipMate/ViewModel/ContentView+ViewModel.swift
+++ ClipMate/ViewModel/ContentView+ViewModel.swift
@@ -12,6 +12,7 @@ extension ContentView {
         @Published var selectedFolder: Folder?
         @Published var editId: String?
         @Published var editText: String = ""
+        @Published var focusClipId: String?
         
         // ClipBoard create
         func create(_ text: String) {
@@ -34,5 +35,33 @@ extension ContentView {
                 FolderUseCases.shared.changeFolderName(name: name, select: select)
             }
         }
+    }
+}
+
+
+// Getter , Setter
+extension ContentView.ViewModel {
+    // focusClip Getter
+    func getFocusClip(_ id: String) {
+        self.focusClipId = id
+    }
+}
+
+extension ContentView.ViewModel {
+    
+    // keyBoard up, down move focusClipID
+    func moveFocus(up: Bool) {
+        guard let folder = selectedFolder else { return }
+        guard let currentId = focusClipId else { return }
+        let sortedClips = folder.clips.sorted { $0.date > $1.date }
+
+        guard !sortedClips.isEmpty else { return }
+        guard let currentIndex = sortedClips.firstIndex(where: { $0.id == currentId }) else { return }
+
+        if up && currentIndex == 0 { return }
+        if !up && currentIndex == sortedClips.count - 1 { return }
+
+        let newIndex = up ? currentIndex - 1 : currentIndex + 1
+        focusClipId = sortedClips[newIndex].id
     }
 }
