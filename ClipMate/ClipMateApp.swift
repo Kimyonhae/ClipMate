69d1fa6b59c1c8218c72d90db51c065febd3f917
김용해 <yoho1019@gmail.com>
2025-08-30 13:40:56 +0900

    feat: ViewModel에 핫키 및 스크린샷 모드 기능 추가

--- ClipMate/ClipMateApp.swift
+++ ClipMate/ClipMateApp.swift
@@ -11,17 +11,12 @@ import HotKey
 @main
 struct ClipMateApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @StateObject private var vm = ContentView.ViewModel()
-    var activeKey: HotKey?
+    @StateObject private var vm: ContentView.ViewModel
+    
     init() {
-        activeKey = HotKey(key: .m, modifiers: [.command])
-        activeKey?.keyDownHandler = {
-            /// **MenuBar를 숨기거나 활성화**
-            /// - 복사할 아이템을 누르면 앱이 다시 비활성화 됨 - contentView
-            /// - command + m 에 등록되어있는 로직이므로 재활용
-            let statusItem = NSApp.windows.first?.value(forKey: "statusItem") as? NSStatusItem
-            statusItem?.button?.performClick(nil)
-        }
+        _vm = StateObject(wrappedValue: ContentView.ViewModel())
+        // Key 적용
+        
     }
     
     var body: some Scene {
